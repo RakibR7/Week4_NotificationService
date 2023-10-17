f2d7e20de9793722ce443be9150a2ce38c11af85
Rakib <G00408644@atu.ie>
2023-10-17 15:33:46 +0100

    base

---
+++ src/main/java/ie/atu/openfeign/OpenfeignApplication.java
@@ -0,0 +1,12 @@
+package ie.atu.openfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenfeignApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OpenfeignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/openfeign/OpenfeignApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.openfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenfeignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
