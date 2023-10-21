809932fca88078f24d57b957f582db5202dbe417
RakibR7 <G00408644@ATU.IE>
2023-10-21 20:33:29 +0100

    Pre_notification working

--- src/main/java/ie/atu/registrationservice/RegistrationController.java
+++ src/main/java/ie/atu/registrationservice/RegistrationController.java
@@ -1,12 +1,8 @@
 package ie.atu.registrationservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class RegistrationController {
